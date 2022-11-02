175a82dc443946bbc3efe007f215def64f9fcbd6
Long Hoang Nguyen <lungnguyenhung@gmail.com>
2022-11-02 22:08:20 +0100

    Removed unnecessary first page

--- clean_website/survey.js
+++ clean_website/survey.js
@@ -10,25 +10,6 @@ var surveyJSON = {
         "name": "page1",
         "elements": [
             {
-            "type": "checkbox",
-            "name": "privacyQuestion",
-            "title": "I have read the privacy statement linked above and agree to have my data processed in the scope of this study.",
-            "isRequired": true,
-            "choices": [
-                {
-                    "value": "item1",
-                    "text": "Yes"
-                }
-            ]
-            }
-        ],
-            "title": "Privacy Statement",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in purus molestie, rhoncus ante ut, faucibus lacus. Etiam imperdiet nisl dui, vitae elementum mi bibendum sed."
-        },
-        {
-        "name": "page2",
-        "elements": [
-            {
             "type": "rating",
             "name": "scaleQuestion",
             "title": "On a scale of one to five, how likely are you going to help the dog?",
@@ -41,7 +22,7 @@ var surveyJSON = {
             "description": "Imagine you are in x situation. How would you rate following statements?"
         },
         {
-            "name": "page3",
+            "name": "page2",
             "elements": [
             {
             "type": "checkbox",
